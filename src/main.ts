a1ad2bd23916f6b96b1b1b6c40e93b8e578c6ba4
At3K1 <odegov06ilya@mail.ru>
2026-04-10 08:44:59 +0700

    add html layot and styles

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,1 @@
 import './style.css';
-
-const app = document.querySelector<HTMLDivElement>('#app')!;
-
-app.innerHTML = `
-  <main class="container">
-    <h1>Лабораторная №2 — Напитки</h1>
-    <p class="hint">Здесь скоро появится список напитков и редактор рецептов.</p>
-  </main>
-`;
